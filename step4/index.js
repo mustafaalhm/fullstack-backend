2fc37307ec0d2edb25aafc7f39057be0ad330a37
mustafaalhm <mustafaalhm@gmail.com>
2024-08-22 22:05:40 +0300

    delete a single phonebook

---
+++ step4/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+
+const app =express();
+app.use(express.json())
+
+const persons =[
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+     return response.json(persons)
+  })
+ 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const result = persons.find((person) => person.id === id)
+    if(!result){
+      response.status(404).send("not found")
+    }
+    response.json(result)
+  })
+ 
+app.get('/info', (request, response) => {
+    response.send(`phonebook has info for ${persons.length}  people <br> <br> ${Date()}`)
+  })
+// delete person 
+app.delete('/api/persons/:id',(req,res)=>{
+  const id = req.params.id;
+ const person = persons.filter(person => person.id !== id);
+
+  if(!person){
+    res.status(404).send("not found")
+  }else{
+    res.send(persons)
+  }
+})
+
+const PORT =3001
+
+app.listen(PORT)
+
